0837f40187f67474ab47eceaaf7d123ef528dba0
antoninoLorenzo <collab.beatbuster@gmail.com>
2024-12-21 16:47:20 +0100

    IP1 - Linux

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,10 @@ namespace fs = std::filesystem;
     #include <sys/types.h>
     #include <sys/wait.h>
 
+    #define READ_END  0
+    #define WRITE_END 1
+    #define BUF_SIZE  256
+
     const char *PATH_DELIMITER = ":";
     const char *PATH_SEPARATOR = "/";
 #elif _WIN32
